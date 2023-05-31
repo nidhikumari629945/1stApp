b8d8781c60eb59e53c46ae935faa4b4ce07d60a7
nidhikumari6299455 <nidhikumari6299455@gmail.com>
2023-05-31 17:54:59 +0530

    changesmade

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './pages/home/home.component';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
 
 const routes: Routes = [
   {
     path:'',
     component:HomeComponent,
+  },
+  {
+    path:'about',
+    component:AboutComponent,
   }
 ];
 
