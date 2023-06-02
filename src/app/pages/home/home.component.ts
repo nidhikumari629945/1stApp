57a25693aeff33eb95f0b65e24b32bcbc5dcef36
nidhikumari6299455 <nidhikumari6299455@gmail.com>
2023-06-02 17:44:06 +0530

    javascript array

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,4 +9,27 @@ export class HomeComponent {
 public data="Hello World..";
 public color="text-primary";
 public data2=false;
+public arraydata=[
+  {
+    name:'aaaaa',
+    id:1,
+  },{
+    name:'bbbbb',
+    id:2,
+  },{
+    name:'ccccc',
+    id:3,
+  },{
+    name:'ddddd',
+    id:4,
+  },
+  {
+    name:'eeeee',
+    id:5,
+  },
+  {
+    name:'fffff',
+    id:5,
+  },
+];
 }
