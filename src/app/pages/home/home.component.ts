b192116b9c52c8a95a6e8c8b871ee00ba167a7ce
nidhikumari6299455 <nidhikumari6299455@gmail.com>
2023-06-04 11:12:36 +0530

    create api and environment folder

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-home',
@@ -8,54 +9,27 @@ import { Component } from '@angular/core';
 export class HomeComponent {
 
  public productDetails: any;
+ public products: any; 
+
 
   public getProductDetail(product: any){
   console.log(product);
-  this.productDetails = product;
+  this.productDetails= product;
+
 
   }
 
+  constructor(public apiService: ApiService) {
+    this.apiService.getProducts().subscribe((productResponse) => {
+      console.log(productResponse);
 
 
-  public products = [
-    {
-      id: 1,
-      name: "Product 1",
-      price: "100",
-      description: "This is a wider card with supporting text below as a natural lead-in to additional content.",
-    imgUrl:'assets/images/home/shirt.jpg',
-    },
-
-    {
-    id: 2,
-      name: "Product 2",
-      price: "150",
-      description: "This is a wider card with supporting text below as a natural lead-in to additional content.",
-      imgUrl:'assets/images/home/SHIRT2.jpg',
-  },
-
-  {
-  id: 3,
-  name: "Product 3",
-      price: "200",
-      description: "This is a wider card with supporting text below as a natural lead-in to additional content.",
-      imgUrl: 'assets/images/home/shirt3.jpg',
-    },
-
-{
-  id: 4,
-  name: "Product 4",
-  price: "250",
-  description: "This is a wider card with supporting text below as a natural lead-in to additional content.",
-  imgUrl: 'assets/images/home/shirt4.jpg',
-},
-
-{
-  id: 5,
-name: "Product 5",
-      price: "300",
-      description: "This is a wider card with supporting text below as a natural lead-in to additional content.",
-imgUrl: 'assets/images/home/shirt5.jpg',
-    },
-    ];
+      this.products= productResponse.data;
+    });
+  }
 }
+
+
+     
+      
+  
